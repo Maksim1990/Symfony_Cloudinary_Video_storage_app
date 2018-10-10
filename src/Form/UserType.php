24cf5ec6d068a20a0e25dafa2d711440b286986e
maxim.narushevich <maxi@digitalmind.be>
2018-10-10 17:17:19 +0300

    - Improved twig base templates layout - Improved Login/Register functionality

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,6 +3,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
@@ -15,13 +16,16 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email', EmailType::class)
-            ->add('username', TextType::class)
+            ->add('email', EmailType::class,array('attr'=>array('class'=>'form-control')))
+            ->add('username', TextType::class,array('attr'=>array('class'=>'form-control')))
             ->add('plainPassword', RepeatedType::class, array(
                 'type' => PasswordType::class,
-                'first_options'  => array('label' => 'Password'),
-                'second_options' => array('label' => 'Repeat Password'),
+                'first_options'  => array('label' => 'Password','attr'=>array('class'=>'form-control')),
+                'second_options' => array('label' => 'Repeat Password','attr'=>array('class'=>'form-control')),
             ))
+            ->add('submit',SubmitType::class,[
+                'attr'=>['class'=>'form-control btn btn-login btn-success']
+            ])
         ;
     }
 
